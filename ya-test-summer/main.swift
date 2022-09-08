dab484654c8c21fc160bd8bf3c4401c55ab8cab3
avedevios <106875633+avedevios@users.noreply.github.com>
2022-09-08 10:45:29 +0300

    Correct

--- ya-test-summer/main.swift
+++ ya-test-summer/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+//let n = 1
+let nString = readLine()!
+let n = Int(nString)!
 
+if n == 1 {
+    print(1)
+} else {
+    print(first(n: n))
+}
+
+func first(n: Int) -> Int {
+    var sum = 0
+    var i = 0
+
+    while sum <= n {
+        i += 1
+        sum += i
+    }
+    
+    return i - 1
+}
